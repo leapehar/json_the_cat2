4ed4fa7407e0282764019357678a30cf63bf8c87
Lea Pehar <leapehar@gmail.com>
2022-01-21 23:36:03 +0000

    refactored the code by creating the fetchBreedDescription function and moving moving the main request logic into it. Module exported fetchBreedDescription function

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,40 +1,63 @@
 // requiring the request library
 const request = require('request');
 
-// declaring breed variable and setting its value to the argument passed in by the user
-const breed = process.argv[2];
 
 
-// declaring endpoint variable and setting its value to the api endpoint
-// using template literal appent the breed variable to the end of the API endpoint so that the endpoint will corespond with the breed the user searched for
-const endpoint = `https://api.thecatapi.com/v1/breeds/search?q=${breed}`;
+// creating fetchBreedDescription function
+const fetchBreedDescription = function(breedName, callback) {
 
+  // declaring endpoint variable and setting its value to the API endpoint (corresponding to a particular breed name)
+  const endpoint = `https://api.thecatapi.com/v1/breeds/search?q=${breedName}`;
 
-// making a GET request to the API endpoint
-request(endpoint, (error, response, body) => {
-
-  // Edge Case: Request Failed
-  // if there is something wrong with the request, console logging an error message and the error
-  if (error) {
-    console.log('Failed request', error);
-    return;
+  // if no breed name value is entered:
+  // calling the callback function (in fetchBreedDescription function call in index.js) with "Please enter breed name" passed into the error parameter
+  // returning whatever the callback function evaluates to
+  if (!breedName) {
+    return callback("Please enter breed name");
   }
+  // making a GET request to the API endpoint 
+  request(endpoint, (error, response, body) => {
 
-  // declaring data variable and getting its value to JSON.parse(body)
-  // JSON.parse() is taking the data that was retrived by the API GET request and converting it from raw JSON into an actual object.
-  const data = JSON.parse(body);
+    // Edge Case: Request Failed
+    // if there is something wrong with the request:
+    // calling the callback function (in fetchBreedDescription function call in index.js)
+    // since no value is passed to the error variable, the error variable will take on the error value that results from the failed AIP request
+    // returning whatever the callback function evaluates to
+    if (error) {
+      return callback(error, body);
+    };
 
-  // Edge Case: Breed Not Found
-  // Checking the character length of the data retrived by the API GET request
-  // if no date is retrived, console logging 'breed not found'
-  if (data.length < 1) {
-    console.log('breed not found');
-    return;
-  }
+    // declaring data variable and getting its value to JSON.parse(body)
+    // JSON.parse() is taking the data that was retrived by the API GET request and converting it from raw JSON into an actual object.
+    const data = JSON.parse(body);
 
-  // declaring description variable and setting its value to the description property of the object retrived by the API GET request.
-  const description = data[0].description;
+    // Edge Case: Breed Not Found
+    // Checking the character length of the data retrived by the API GET request
+    // if no date is retrived, console logging 'breed not found'
+    // if (data.length < 1) {
+    //   // console.log('breed not found')
+    //   return callback(error, null);
+    // };
 
-  // console logging the breed description
-  console.log(`${breed} Cat Description: ${description}`);
-});
+    // declaring desc variable and setting its value to the first index of the array retrived by the API GET request.
+    const desc = data[0];
+
+    // checking if desc exists by checking if it evaluates to true 
+    // if desc evaluated to true, calling the callback function (in fetchBreedDescription function call in index.js) with the description property on desc(which is the object at the first index of the array retrived by the API GET request)
+    //returning what the callback evaluates to, ie. the breed description
+    if (desc) {
+      return callback(error, desc.description);
+    } else {
+      // if desc evaluates to false, calling the callback function (in fetchBreedDescription function call in index.js) with "No breed found" passed into the error parameter
+      //returning whatever the callback evalutes to 
+      return callback("No breed found")
+    }
+
+    // console logging the breed description
+    // console.log(`${breed} Cat Description: ${description}`);
+  });
+
+};
+
+// exporting fetchBreedDescription function 
+module.exports = {fetchBreedDescription};
